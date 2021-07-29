1d1bf1b7bc3976d9c51c72aa034228a84eadd543
cristina-fierro <jazmin.fierro@senirop.com>
2021-07-28 21:35:44 -0500

    Added articles description column

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_28_023045) do
+ActiveRecord::Schema.define(version: 2021_07_29_022326) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at"
+    t.text "description"
   end
 
 end
