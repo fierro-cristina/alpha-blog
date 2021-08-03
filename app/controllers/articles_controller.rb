c20e7bd1e19e7c1a9c26414506b6782c09fa7886
cristina-fierro <jazmin.fierro@senirop.com>
2021-08-02 20:30:35 -0500

    Created article index

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,14 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def index
+        @articles = Article.all
+    end
+
+    def new
+    end
+
+    def create
+    end
+
 end
