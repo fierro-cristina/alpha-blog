cf8388efa9ff046880fc480d87e258192c59716c
cristina-fierro <jazmin.fierro@senirop.com>
2021-08-03 20:26:35 -0500

    Created new article form with redirect

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,11 @@ class ArticlesController < ApplicationController
     end
 
     def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article.save
+        redirect_to article_path(@article) #this will get /article/:id path
+        #the above line is the same as 'redirect_to @article'
+        
     end
 
 end
