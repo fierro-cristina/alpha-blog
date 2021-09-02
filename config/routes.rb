5b8b42e728ea5c8e6de7c09cee2cdd0dc1a2ff30
cristina-fierro <jazmin.fierro@senirop.com>
2021-09-01 20:03:38 -0500

    Added link to routes inside the project

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+
+  # The following line represents ALL the routes for articles
+  # These routes are called REST - Representational state transfer, mapping HTTP
+  # verbs (get, post, put/patch, delete) to CRUD actions.
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  #The preceeding line can be replaced as follows:
+  resources :articles
+
 end
