c84b88a37df137675004f6e7a5af805ef5b13db7
cristina-fierro <jazmin.fierro@senirop.com>
2022-01-20 00:41:55 -0500

    Created user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_29_022326) do
+ActiveRecord::Schema.define(version: 2022_01_20_053542) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at"
     t.text "description"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
