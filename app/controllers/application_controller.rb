f16117c549a0822fe5b4a15d149d6f26306c814b
fierro-cristina <jazmin.fierro@senirop.com>
2022-08-31 21:00:43 -0500

    Latest updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,12 +18,12 @@ class ApplicationController < ActionController::Base
     end
 
     def same_user_required
-        if @article.present? && current_user != @article.user
+        if @article.present? && current_user != @article.user && !current_user.admin?
             flash[:notice] = "You cannot edit this article."
             redirect_to @article
         end
 
-        if @user.present? && current_user != @user
+        if @user.present? && current_user != @user && !current_user.admin?
             flash[:notice] = "You cannot edit this user's profile."
             redirect_to @user
         end
